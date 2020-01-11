6be61c88c9900e3b66833b0e6ea94e84b0044d0b
Michael Li <shaznl1@hotmail.com>
2020-01-11 07:22:56 -0500

    Update Tensor Completion

--- src/MatrixImpute.jl
+++ src/MatrixImpute.jl
@@ -6,22 +6,37 @@ using StatsBase: sample
 using Arpack: svds
 using DataFrames: allowmissing
 using Statistics: mean
+using TensorToolbox: ktensor
 
 
 
 function Impute(A,k;method=:fastImpute,γ = 10e4, λ=0.1, B=nothing, lr= 1/64)
 
-if method == :fastImpute
-    Uopt, Sopt = fastImpute(A,k,γ,lr,B)
-    return Uopt, Sopt
-else
-    error("Method not yet implemented!")
+    dims = length(size(A))
+
+    if dims == 2
+        if method == :fastImpute
+            Aopt = fastImpute(A,k,γ,lr,B, approx = true)
+            return Aopt
+        else
+            error("Method not yet implemented!")
+        end
+    elseif dims ==3
+        if method == :fastImputeT && B == nothing
+            Aopt = fastImputeT(A,k,γ,lr)
+            return Aopt
+        elseif method == :fastImputeT
+            error("Tensor with side information is not supported!")
+        else
+            error("Method not yet implemented!")
+        end
+    else
+        error("Number of Dimensions Incorrect")
+    end
 end
 
-end
 
-
-function fastImpute(A,k,γ,lr,B)
+function fastImpute(A,k,γ,lr,B; approx = true)
 # Unknown values should be in missing for A
 # A is the matrix that needs to be imputed, size n x m
 # B is the feature selector, size m x p
@@ -38,26 +53,28 @@ function fastImpute(A,k,γ,lr,B)
         Uopt = zeros(Float64,n,k)
         Sopt = zeros(Float64,k,p)
         Aopt = zeros(Float64,n,p)
-        t=0
         if nchunks>1
             for sc in startchunk
                 UoptT, SoptT = fastImputeInner(A[:,sc:min(sc+chunk-1,p)],k,γ,lr,B)
                 Aopt[:,sc:min(sc+chunk-1,p)] = UoptT * SoptT
             end
-            output = svds(Aopt, nsv = k)
-            Soptt = diagm(output[1].S)*output[1].Vt
-            scale = norm(Soptt)
-            return output[1].U .* scale, Soptt ./ scale
+            if approx
+                return Aopt
+            else
+                output = svds(Aopt, nsv = k)
+                Aopt = output[1].U*diagm(output[1].S)*output[1].Vt
+                return Aopt
+            end
         else
             Uopt, Sopt = fastImputeInner(A,k,γ,lr,B)
-            return Uopt, Sopt
+            return Uopt * Sopt
         end
     else
         if m != size(B)[1]
             error("Sizes of A and B must match")
         end
         Uopt, Sopt = fastImputeInner(A,k,γ,lr,B)
-        return Uopt, Sopt
+        return Uopt * Sopt
     end
 end
 
@@ -123,8 +140,6 @@ function fastImputeInner(A,k,γ,lr,B)
             objbest = obj0
             raised = false
         end
-        println("There are $noimprov no improvement iterations")
-        println("Estimated objective is: $obj0")
         i = i + 1
     end
     Sopt = S
@@ -169,15 +184,11 @@ function MatrixDeriv(A,W,S,k,γ,M,j)
     obj = @distributed (+) for i = 1:nnew
         dot(Apar[i],objpar[i]) / normfactor
     end
-    println("Starting DerivCalc")
-    println("We chose $nnew n")
-    println("We chose $mnew m")
     Btemp2 = zeros(k,nnew)
     for i = 1:nnew
         Btemp2[:,i] = Xpar[i]' * objpar[i]
         ∇obj[:,samplem[Wpar[i].==1]] += -2 * γ * Btemp2[:,i] * objpar[i]' / normfactor
     end
-    println("Derivative Calculated")
     return obj, ∇obj
 end
 
@@ -204,9 +215,6 @@ function MatrixDeriv2(A,B,W,S,k,γ,M,j)
     obj = @distributed (+) for i = 1:nnew
         dot(Apar[i],objpar[i]) / (2*mnew*nnew)
     end
-    println("Starting DerivCalc")
-    println("We chose $nnew n")
-    println("We chose $mnew m")
     Btemp1 = zeros(p,nnew)
     Btemp2 = zeros(k,nnew)
     for i = 1:nnew
@@ -216,8 +224,128 @@ function MatrixDeriv2(A,B,W,S,k,γ,M,j)
     ∇obj = @distributed (+) for i = 1:nnew
         -2 * γ * Btemp2[:,i] * Btemp1[:,i]' / (2*mnew*nnew)
     end
-    println("Derivative Calculated")
     return obj, ∇obj
+end
+
+function fastImputeT(A,k,γ,lr)
+# Unknown values should be in missing for A
+# A is the tensor that needs to be imputed, size n x m x p
+# k is the maximum CP rank of the resultant matrix
+# γ is the regularization parameter
+# lr is the learning rate of the gradient descent function
+    n,m,p = size(A)
+    Amat = reshape(A,(n*m,p))
+    W = ones(n,m,p)-ismissing.(A)
+    Wmat = ones(n*m,p)-ismissing.(Amat)
+    x = rand(n, k)
+    y = rand(m, k)
+    M = sum(ismissing.(A))/(n*m*p)
+
+    z = [x; y]
+    z = z ./ norm(z)
+    x = z[1:n,:]
+    y = z[n+1:end,:]
+    i = 0
+    j = 0
+    println("Preprocessing Complete")
+    obj0, ∇obj0x, ∇obj0y = TensorDeriv(Amat,Wmat,x,y,k,γ,M,j,size(A))
+
+    ∇obj0 = [∇obj0x; ∇obj0y]
+    ∇obj = zeros(m+n,k)
+
+    objbest = Inf
+    noimprov = 0
+    raised = false
+    while i<50
+        if (noimprov>=5) && (!raised)
+            j = j + 1
+            raised = true
+            objbest = obj0
+            noimprov = 0
+        end
+        ∇obj = ∇obj0 + i / (i+3) * ∇obj
+        ∇tan = - ∇obj + dot(∇obj,z) * z
+        updategrad = ∇tan / norm(∇tan)
+        z = z .* cos(pi * lr) + updategrad * sin(pi * lr)
+        z = z ./ norm(z)
+        x = z[1:n,:]
+        y = z[n+1:end,:]
+        obj0, ∇obj0x, ∇obj0y = TensorDeriv(Amat,Wmat,x,y,k,γ,M,j,size(A))
+        ∇obj0 = [∇obj0x; ∇obj0y]
+        if obj0 > objbest
+            noimprov = noimprov + 1
+            if (noimprov % 5 == 0) && (raised)
+                raised = false
+            end
+        else
+            noimprov = 0
+            objbest = obj0
+            raised = false
+        end
+        i = i + 1
+    end
+    X=zeros(m*n,k)
+    function findbeta(Xrow, Arow)
+        return inv(I/γ+Xrow'*Xrow)*Xrow'*Arow
+    end
+    for i=1:k
+        X[:,i]=reshape(x[:,i]*y[:,i]',n*m)
+    end
+    function PopulateA(Xtemp,Atemp)
+        return X*(pinv(Xtemp'*Xtemp,1e-7)*(Xtemp'*Atemp))
+    end
+    Aopt=zeros(n,m,p)
+    Wpar=Array{Float64}[Wmat[:,i] for i=1:p]
+    Xpar=Array{Float64}[X[Wpar[i].==1,:] for i=1:p]
+    Apar=Array{Float64}[Amat[Wpar[i].==1,i] for i=1:p]
+    result=map(PopulateA,Xpar,Apar)
+    println("Model Solved")
+    for i=1:p
+        Aopt[:,:,i]=reshape(result[i],(n,m))
+    end
+    return Aopt
+end
+
+function TensorDeriv(Amat,Wmat,x,y,k,γ,M,j,dim)
+    n,m,p = dim
+    ncube = (n*m*p)^(1/3)
+    nsamples = ncube * k * log(ncube)^2 * 4 * 2^j / (1-M)
+    α = nsamples / (n*m*p)
+    pnew = min(max(100, Int(round(α * p))), p)
+    samplep = sample(1:p,pnew,replace=false)
+    normfactor = n * m * pnew
+    function SmallInv(Xrow, Wrow, Arow)
+        return Arow- Xrow*(inv(I/γ+Xrow'*Xrow)*(Xrow'*Arow))
+    end
+    X = zeros(n*m,k)
+    for i=1:k
+        X[:,i] = reshape(x[:,i]*y[:,i]',n*m)
+    end
+    Wpar=Array{Float64}[Wmat[:,samplep[i]] for i=1:pnew]
+    Xpar=Array{Float64}[X[Wpar[i].==1,:] for i=1:pnew]
+    Apar=Array{Float64}[Amat[Wpar[i].==1,samplep[i]] for i=1:pnew]
+    objpar=map(SmallInv,Xpar,Wpar,Apar)
+    obj=@distributed (+) for i=1:pnew
+        dot(Apar[i],objpar[i]) / normfactor
+    end
+    ∇objx=zeros(n,k)
+    ∇objy=zeros(m,k)
+    ∇obj=zeros(n*m,k)
+    # Btemp1 = zeros(p, k)
+    for i = 1:pnew
+        # Btemp1[i, :] = objpar[i]' * Xpar[i]
+        ∇obj[Wpar[i].==1,:] += -2 * γ * objpar[i] * (objpar[i]' * Xpar[i]) / normfactor
+    end
+
+    ∇obj = reshape(∇obj, (n,m,k))
+    for i = 1:k
+        ∇objx[:,i]=∇obj[:,:,i] * y[:,i]
+    end
+    for i = 1:k
+        ∇objy[:,i]=∇obj[:,:,i]' * x[:,i]
+    end
+
+    return obj, ∇objx, ∇objy
 end
 
 export Impute
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
+using DataFrames, TensorToolbox, MatrixImpute
+
 n=10^5
 m=10^3
-k=5
+k=7
 U=rand(Float64,n,k)
 V=rand(Float64,k,m)
 Afull=U*V
@@ -8,16 +10,14 @@ A=copy(Afull)
 A = allowmissing(A)
 A[rand(Float64,n,m).>0.05].=missing
 t1 = time_ns()
-Uopt, Sopt = Impute(A,k,γ = 10000)
-println((time_ns()-t1)/1e9)
-println(mean(abs.(Uopt*Sopt-Afull)./abs.(Afull)))
+Aopt = MatrixImpute.Impute(A,k,γ = 10000)
 println("Completed Test for Matrix Completion without Side Information")
 
 
 n=10^3
 m=10^3
 p=100
-k=5
+k=3
 U=rand(Float64,n,k)
 R=rand(Float64,k,p)
 B=rand(Float64,p,m)
@@ -26,6 +26,25 @@ A=copy(Afull)
 A = allowmissing(A)
 A[rand(Float64,n,m).>0.05].=missing
 t1 = time_ns()
-Uopt, Sopt = MatrixImpute.Impute(A,k,γ = 10000, B = B')
-println(mean(abs.(Uopt*Sopt*B-Afull)./abs.(Afull)))
+Aopt = MatrixImpute.Impute(A,k,γ = 10000, B = B')
 println("Completed Test for Matrix Completion with Side Information")
+
+n=100
+m=200
+p=500
+k=6
+M=0.8
+γ=1000000
+stepsize=64
+x=rand(n,k)
+y=rand(m,k)
+z=rand(p,k)
+B=[rand(n,k),rand(m,k),rand(p,k)];
+A=full(ktensor(B))
+Afull=copy(A)
+Afullmat = reshape(Afull,(n*m,p))
+A=allowmissing(A)
+A[rand(n,m,p).>1-M].=missing
+t1 = time_ns()
+Aopt = MatrixImpute.Impute(A,k,γ = γ, method = :fastImputeT)
+println("Completed Test for Tensor Completion")
